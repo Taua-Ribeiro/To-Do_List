17690843e047db20b2c6305ce3812a71f896721d
Taua-Ribeiro <tauaribeiro.ba1@gmail.com>
2026-06-05 20:09:53 -0300

    UPDATE: db_config.py; DELETE: db_test_config.py, factorys/
    Alterando o modo de conexão para o banco de dados para uma forma mais simples.

--- src/config/db_config.py
+++ src/config/db_config.py
@@ -1,27 +1,18 @@
 import sqlite3
-from src.config.tables_config import TABLES_SCRIPTS
-import os
-class ConexaoProducao:
-    """
-    Classe resposável por fazer a conexão com o banco de dados de produção.
-    """
+from src.scripts.tables_scripts import TABLES_SCRIPTS
+from contextlib import contextmanager
 
+@contextmanager
+def conectar(eTeste: bool = False):
+    database = "test.db" if eTeste else "database.db"
 
-    @staticmethod
-    def conectar() -> sqlite3.Connection:
-        """
-        Conecta ao banco de dados e retorna um objeto do tipo Connection
-        """
+    conexao = sqlite3.connect(database, uri= eTeste)
 
+    conexao.execute("PRAGMA foreign_keys = ON")
 
-        conexao = sqlite3.connect("database.db")
+    for SCRIPT in TABLES_SCRIPTS:
+        conexao.execute(SCRIPT)
 
-        conexao.execute("PRAGMA foreign_keys = ON")
+    yield conexao
 
-
-        for SCRIPT in TABLES_SCRIPTS:
-            conexao.executemany(SCRIPT)
-
-        conexao.commit()
-
-        return conexao
+    conexao.close()
