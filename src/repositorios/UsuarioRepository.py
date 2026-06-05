3ea24d652cf210e7e28707835a6a0aa11c156350
Taua-Ribeiro <tauaribeiro.ba1@gmail.com>
2026-06-05 20:11:12 -0300

    UPDATE: UsuarioRepository.py
    Adaptando o repositório de ususario para utilizar a nova forma de conexão.

--- src/repositorios/UsuarioRepository.py
+++ src/repositorios/UsuarioRepository.py
@@ -1,66 +1,91 @@
 from src.model.Usuario import Usuario
-from src.config.factorys.db_factory import ConectarFactory
+from src.config.db_config import conectar
 from datetime import datetime
 from uuid import uuid4
 
 class UsuarioRepository:
-    @staticmethod
-    def cadastrar_usuario(usuario: Usuario, eTeste: bool= False) -> None:
-        con = ConectarFactory.criarConexao(eTeste).conectar()
-        cursor = con.cursor()
-
-        
-        cursor.execute("INSERT INTO Usuario(id, nome, email, senha) VALUES(?,?,?,?)", (usuario.id, usuario.nome, usuario.email, usuario.senha))
-
-        con.commit()
-        con.close()
+    _eTeste = False
     
-    @staticmethod
-    def find_usuario(usuario: Usuario|None, eTeste: bool= False) -> Usuario|List[Usuario]:
-        con = ConectarFactory.criarConexao().conectar(eTeste)
-        cursor = con.cursor()
+    @classmethod
+    def utilizar_conexao_teste(cls):
+        cls._eTeste = True
 
-        if usuario:
-            res = cursor.execute("SELECT id, nome, email, senha FROM Ususario WHERE (email = ?);", (usuario.email))
-            nome, email, senha = res.fetchone()
+    @classmethod
+    def cadastrar_usuario(cls, usuario: Usuario) -> None:
+        try:
+            with conectar(cls._eTeste) as con:
+                cursor = con.cursor()
 
-            return Usuario(nome, email, senha)
-        
-        res = cursor.execute("SELECT nome, email, senha FROM Usuario")
+                
+                cursor.execute("INSERT INTO Usuario(id, nome, email, senha) VALUES(?,?,?,?)", (str(usuario.id), usuario.nome, usuario.email, usuario.senha))
+                
+                con.commit()
+        except Exception as erro:
+            con.rollback()
+            raise(erro)
 
-        return [Usuario(l.nome, l.email, l.senha) for l in res.fetchall()]
+    @classmethod
+    def find_usuario(cls, email: str|None= None):
+        with conectar(cls._eTeste) as con:
+            try:
+                cursor = con.cursor()
+                if email:
+                    res = cursor.execute("SELECT id, nome, senha FROM Usuario WHERE (email = ?);", (email,))
+                    id, nome, senha = res.fetchone()
 
-    @staticmethod
-    def editar_usuario(usuario_editado: Usuario, eTeste: bool= False) -> None:
-        con = ConectarFactory.criarConexao(eTeste).conectar()
-        cursor = con.cursor()
+                    return Usuario(nome, email, senha, id= id)
+                
+                res = cursor.execute("SELECT id, nome, email, senha FROM Usuario")
 
-        res = cursor.execute("SELECT senhaFROM Usuario WHERE (id = ?)",(usuario_editado.id))
+                # cursor.execute("DELETE FROM Usuario;")
+                print(cursor.execute("SELECT count(*) AS numero_linhas FROM Usuario;").fetchall())
+                
+                resultado = list(map(lambda tuple: Usuario(tuple[1], tuple[2], tuple[3], id= tuple[0]), res.fetchall()))
 
-        senha_antiga = res.fetchone()[0]
+                return resultado
+            except Exception as erro:
+                con.rollback()
+                raise(erro)
 
-        cursor.execute("""
-                       UPDATE USUARIO SET(
-                       nome = ?, 
-                       email = ?, 
-                       senha = ?, 
-                       atualizado_em = ?),
-                       WHERE (id = ?);
-                       """, (usuario_editado.nome, usuario_editado.email, usuario_editado.senha, datetime.now(), usuario_editado.id))
-        
-        if senha_antiga != usuario_editado.senha:
-            cursor.execute("INSERT INTO Senhas_Usuario(id, id_usuario, ultima_senha) VALUES(?,?,?)"(uuid4(), usuario_editado.id, senha_antiga))
+    @classmethod
+    def editar_usuario(cls, usuario_editado: Usuario) -> None:
+        with conectar(cls._eTeste) as con:
+            try:
+                cursor = con.cursor()
 
-        con.commit()
-        con.close()
+                res = cursor.execute("SELECT senhaFROM Usuario WHERE (id = ?)",(usuario_editado.id))
 
-    def remover_usuario(usuario: Usuario, eTeste: bool= False) -> None:
-        con = ConectarFactory.criarConexao(eTeste).conectar()
-        cursor = con.cursor()
+                senha_antiga = res.fetchone()[0]
 
-        cursor.execute("DELETE FROM Usuario WHERE (id = ?)", (usuario.id))
+                cursor.execute("""
+                            UPDATE USUARIO SET(
+                            nome = ?, 
+                            email = ?, 
+                            senha = ?, 
+                            atualizado_em = ?),
+                            WHERE (id = ?);
+                            """, (usuario_editado.nome, usuario_editado.email, usuario_editado.senha, datetime.now(), usuario_editado.id))
+                
+                if senha_antiga != usuario_editado.senha:
+                    cursor.execute("INSERT INTO Senhas_Usuario(id, id_usuario, ultima_senha) VALUES(?,?,?)", (str(uuid4()), str(usuario_editado.id), senha_antiga))
 
-        con.commit()
-        con.close()
+                con.commit()
+            except Exception as erro:
+                con.rollback()
+                raise(erro)
+    
+    @classmethod
+    def remover_usuario(cls, usuario: Usuario) -> None:
+        with conectar(cls._eTeste) as con:
+            try:
+                cursor = con.cursor()
 
+                cursor.execute("DELETE FROM Usuario WHERE (id = ?)", (usuario.id))
+
+                con.commit()
+            except Exception as erro:
+                con.rollback()
+                raise(erro)
+
+    
         
