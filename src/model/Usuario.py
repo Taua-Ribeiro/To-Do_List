7438fe6ec741f2ace7b4d1d6e36ca1ff711245b6
Taua-Ribeiro <tauaribeiro.ba1@gmail.com>
2026-06-08 19:18:53 -0300

    UPDATE: Usuario.py
    Atualizando o model de usuario de usuario para utilizar dataClass

--- src/model/Usuario.py
+++ src/model/Usuario.py
@@ -1,31 +1,31 @@
 import re
 from uuid import uuid4, UUID
+from dataclasses import dataclass, field
 
+@dataclass()
 class Usuario:
-    def __init__(self, nome: str, email: str, senha: str, id: str| UUID= None):
+    _nome: str
+    _email: str
+    _senha: str
+    _id: UUID = field(default_factory = uuid4)
+
+    def __post_init__(self):
         erros = []
 
-        if len(nome.strip()) < 3:
+        if len(self._nome.strip()) < 3:
             erros.append(ValueError("Nome não pode ser menor que 3 caractres."))
         
-        if not re.fullmatch(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",email.strip()):
+        if not re.fullmatch(r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$",self._email.strip()):
             erros.append(ValueError("Email inválido"))
 
-        if len(senha) < 8:
+        if len(self._senha) < 8:
             erros.append(ValueError("A senha deve ter no mínimo 8 caracteres"))
         
         if len(erros):
             raise ExceptionGroup("Erro de validação", erros)
 
-        if id:
-            self._id = UUID(id)
-        else:
-            self._id = uuid4()
-
-        self._nome = nome.strip()
-        self._email = email.strip().lower()
-        self._senha = senha
-    
+        self._id = UUID(self._id) if isinstance(self._id, str) else self._id
+            
     @property
     def nome(self) -> str:
         return self._nome
@@ -56,15 +56,16 @@ class Usuario:
     def senha(self, senha: str) -> None:
         if len(senha) < 8:
             raise ValueError("A senha deve ter no mínimo 8 caracteres")
+
+        self._senha = senha
     
     @property
     def id(self) -> UUID:
-        return self._id      
-
-    def __eq__(self, other):
-        return self._id == other._id
+        return str(self._id)
     
-    def __repr__(self):
-        return f'(id: {str(self._id)}, nome: {self._nome}, email: {self.email})'
+    @id.setter
+    def id(self, id: str|UUID) -> None:
+        if isinstance(id, str):
+            id = UUID(id)
 
-    
+        self._id = UUID(id)
