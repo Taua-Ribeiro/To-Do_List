9fb5722a36e439e4c2376fe594c364f154c806f0
Taua Ribeiro <tauaribeiro.ba1@gmail.com>
2026-07-31 21:18:36 -0300

    UPDATE
    Atualizando o 'create_usuario' para utilizar o session corretamente.

--- app/services/usuario_service.py
+++ app/services/usuario_service.py
@@ -1,26 +1,27 @@
-from sqlalchemy import MetaData, Table
-
 from flask import current_app
 
-from app.database import get_session, load_tables
+from app.database import get_session
 from werkzeug.security import generate_password_hash, check_password_hash
 
-@load_tables
+from app.database.models import Usuarios
+
+
 def create_usuario(nome: str, senha: str):
-    with get_session() as session:
-        hash_senha = generate_password_hash(senha)
+    with current_app.app_context():
+        with get_session() as session:
+            hash_senha = generate_password_hash(senha)
 
-        novo_usuario = Usuarios(nome= nome, hash_senha= hash_senha)
+            novo_usuario = Usuarios(nome= nome, hash_senha= hash_senha)
 
-        session.add(novo_usuario)
+            session.add(novo_usuario)
 
-        session.commit()
+            session.commit()
     
 
 def get_all_usuarios():
     pass
 
-def get_one_usuario(id: Int):
+def get_one_usuario():
     pass
 
 def update_usuario():
