7953ed95a7647b7e5f2cc4f7baba2e099b6e1ff5
Taua Ribeiro <tauaribeiro.ba1@gmail.com>
2026-08-03 22:41:31 -0300

    UPDATE
    Adicionando a função que testa o find_one do service de usuários

--- tests/usuarios_service_test.py
+++ tests/usuarios_service_test.py
@@ -1,4 +1,4 @@
-from app.services.usuario_service import create_usuario
+from app.services.usuario_service import create_usuario, get_one_usuario
 
 from app.database import get_connection
 
@@ -14,6 +14,14 @@ def test_register_usuario(app):
             assert "novo_usuario" == result.nome
             assert "teste" != result.hash_senha
 
+def test_search_usuario(app):
+    with app.app_context():
+        result = get_one_usuario("teste")
 
+        with get_connection() as conn:
+            usuario_teste = conn.execute(text("SELECT * FROM Usuarios WHERE id=1;")).fetchone()
+
+        assert result.id == usuario_teste.id
+        assert result.nome == usuario_teste.nome
 
 
