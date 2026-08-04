0b72c7b86aae334040a1018558828034011ed288
Taua Ribeiro <tauaribeiro.ba1@gmail.com>
2026-08-03 22:18:28 -0300

    UPDATE
    Criando a classe base das excessões da aplicação e da excessão relacionada aos services.

--- app/utils/app_exceptions.py
+++ app/utils/app_exceptions.py
@@ -1,6 +1,16 @@
-class DatabaseException(Exception):
-    """Erros relacionados ao banco de dados
+class AppException(Exception):
+    """Classe base para os erros relacionados à aplicação.
     """
     def __init__(self, message, original_error):
-        self.original_error = original_error
-        super().__init__(message)
+            self.original_error = original_error
+            super().__init__(message)
+    
+class DatabaseException(AppException):
+    """Erros relacionados ao banco de dados
+    """
+    pass
+
+class ServiceException(AppException):
+    """Erros relacionados aos services do sistema.
+    """
+    pass
