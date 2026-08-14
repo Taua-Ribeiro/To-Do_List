78a4aeba63414831cabc9b000495baa9148c8d89
Taua Ribeiro <tauaribeiro.ba1@gmail.com>
2026-08-13 22:28:30 -0300

    Adicionando a view de login e adicionando tratamento extra para a view de registro

--- app/auth.py
+++ app/auth.py
@@ -11,12 +11,15 @@ from flask import (
     url_for,
     abort
 )
+from werkzeug.security import check_password_hash
 
 from app.database import get_session
+from app.services.usuario_service import create_usuario, get_one_usuario
+from app.utils.app_exceptions import ServiceException
 
 from sqlalchemy import select
 
-from app.services.usuario_service import create_usuario
+
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -45,35 +48,62 @@ def register():
     method = request.method
     error = []
 
-    if method == 'POST':
-        login = request.form["login"].strip()
-        senha = request.form["senha"].strip()
-        confirmacao = request.form["confirmacao"].strip()
+    try:
+        if method == 'POST':
+            login = request.form["login"].strip()
+            senha = request.form["senha"].strip()
+            confirmacao = request.form["confirmacao"].strip()
 
-        if not login:
-            error.append("Login é necessário!")
+            if not login:
+                error.append("Login é necessário!")
 
-        if not senha:
-            error.append("Senha é necessária!")
+            if not senha:
+                error.append("Senha é necessária!")
 
-        if len(senha) < 4:
-            error.append("A senha precisa ter pelo menos 4 caracters!")
+            if len(senha) < 4:
+                error.append("A senha precisa ter pelo menos 4 caracters!")
 
-        if senha != confirmacao:
-            error.append("A senha não é igual à confirmação da senha!")
+            if senha != confirmacao:
+                error.append("A senha não é igual à confirmação da senha!")
 
-        if len(error) == 0:
-            create_usuario(login, senha)
+            if len(error) == 0:
+                create_usuario(login, senha)
 
-            flash("Usuário criado com sucesso!", "sucesso")
+                flash("Usuário criado com sucesso!", "sucesso")
 
-            return redirect(url_for('auth.login'))
-    elif method != "GET":
-        return abort(405, f'Método {method} não é permitido!')
+                return redirect(url_for('auth.login'))
+        elif method != "GET":
+            return abort(405, f'Método {method} não é permitido!')
 
-    print(error)
-    return render_template('auth/register.html', error=error)
+        return render_template('auth/register.html', error=error)
+    except ServiceException as e:
+        return render_template('auth/register.html', error= [e.message])
+
 
 @bp.route("/login", methods=["GET", "POST"])
 def login():
-    return "Funcionou"
+    error = []
+
+    if request.method == "POST":
+        login = request.form["login"].strip()
+        senha = request.form["senha"].strip()
+
+        if not login:
+            error.append("O login é necessário!")
+
+        if not senha:
+            error.append("A senha é necessária!")
+
+        try:
+            usuario = get_one_usuario(login)
+
+            if not check_password_hash(usuario.hash_senha, senha):
+                error.append("Login e/ou senha inválido(os)!")
+        except ServiceException as e:
+            error.append(e.message)
+
+
+        if len(error) == 0:
+            return "Login efetuado!"
+
+    return render_template('auth/login.html', error= error)
