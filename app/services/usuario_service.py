a3b1a559e718ee31475ae764b4669e0384b47499
Taua Ribeiro <tauaribeiro.ba1@gmail.com>
2026-08-13 22:31:02 -0300

    UPDATE
    Adicionando tratamento para os erros de integridade e de busca do banco de dados.

--- app/services/usuario_service.py
+++ app/services/usuario_service.py
@@ -7,30 +7,32 @@ from app.database.models import Usuarios
 from app.utils.app_exceptions import ServiceException
 
 import sqlalchemy as db
+from sqlalchemy.exc import IntegrityError, NoResultFound
 
 def create_usuario(nome: str, senha: str):
-    with current_app.app_context():
-        with get_session() as session:
-            hash_senha = generate_password_hash(senha)
+    try:
+        with current_app.app_context():
+            with get_session() as session:
+                hash_senha = generate_password_hash(senha)
 
-            novo_usuario = Usuarios(nome= nome, hash_senha= hash_senha)
+                novo_usuario = Usuarios(nome= nome, hash_senha= hash_senha)
 
-            session.add(novo_usuario)
+                session.add(novo_usuario)
 
-            session.commit()
-    
+                session.commit()
+    except IntegrityError as e:
+        raise ServiceException("Usuário já existe!", e)    
 def get_one_usuario(nome: str) -> Usuarios:
-    with current_app.app_context():
-        with get_session() as session:
-            stmt = db.Select(Usuarios).where(Usuarios.nome == nome)
+    try:
+        with current_app.app_context():
+            with get_session() as session:
+                stmt = db.Select(Usuarios).where(Usuarios.nome == nome)
 
-            usuario = session.scalars(stmt).one()
+                usuario = session.scalars(stmt).one()
 
-        if usuario is None:
-            raise ServiceException('Usuário não encontrado.')
-
-        return usuario
-        
+            return usuario
+    except NoResultFound as e:
+        raise ServiceException("Usuário e/ou senha inválidos!", e)
 
 def update_usuario():
     pass
