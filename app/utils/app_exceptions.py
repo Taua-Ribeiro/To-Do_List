690677fac678b2d1d5934dbed4376830ee775910
Taua Ribeiro <tauaribeiro.ba1@gmail.com>
2026-08-13 22:29:43 -0300

    DEBUG
    Fazendo com que AppException tenha o atributo message.

--- app/utils/app_exceptions.py
+++ app/utils/app_exceptions.py
@@ -1,8 +1,9 @@
 class AppException(Exception):
     """Classe base para os erros relacionados à aplicação.
     """
-    def __init__(self, message, original_error):
+    def __init__(self, message: str, original_error: Exception):
             self.original_error = original_error
+            self.message = message
             super().__init__(message)
     
 class DatabaseException(AppException):
