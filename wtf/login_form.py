e95145931b6b2cfd27592c4858ae3992c32c6154
Taua Ribeiro <tauaribeiro.ba1@gmail.com>
2026-08-23 18:35:27 -0300

    UPDATE
    Adicionando a menssagem de erro.

--- wtf/login_form.py
+++ wtf/login_form.py
@@ -3,5 +3,5 @@ from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired,Length
 
 class LoginForm(FlaskForm):
-    login = StringField('Login', validators=[DataRequired()])
-    senha = PasswordField('Senha', validators=[DataRequired(), Length(min=4, message='A senha precisa ter pelo menos 4 dígitos')])
+    login = StringField('Login', validators=[DataRequired(message= 'Login é obrigatório!')])
+    senha = PasswordField('Senha', validators=[DataRequired(message='A senha é obrigatória'), Length(min=4, message='A senha precisa ter pelo menos 4 dígitos')])
