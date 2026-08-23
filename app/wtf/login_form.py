c074221bf0fa161c0fa9a1e15a1d1ffa5b3c16a3
Taua Ribeiro <tauaribeiro.ba1@gmail.com>
2026-08-23 20:02:26 -0300

    UPDATE
    Adicionando o botão de submit ao formulário.

--- app/wtf/login_form.py
+++ app/wtf/login_form.py
@@ -1,7 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired,Length
+from wtforms import SubmitField
 
 class LoginForm(FlaskForm):
     login = StringField('Login', validators=[DataRequired(message= 'Login é obrigatório!')])
-    senha = PasswordField('Senha', validators=[DataRequired(message='A senha é obrigatória'), Length(min=4, message='A senha precisa ter pelo menos 4 dígitos')])
+    senha = PasswordField('Senha', validators=[DataRequired(message='A senha é obrigatória'), 
+                            Length(min=4, message='A senha precisa ter pelo menos 4 dígitos')])
+
+    submit = SubmitField('Entrar')
