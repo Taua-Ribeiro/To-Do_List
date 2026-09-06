877d08f837542161c102a563bb4fc2f24dd8948d
Taua Ribeiro <tauaribeiro.ba1@gmail.com>
2026-09-06 10:12:59 -0300

    CREATE
    Criando o formulário de registro.

---
+++ app/wtf/register_form.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+class RegisterForm(FlaskForm):
+    login = StringField("Login", validators=[DataRequired(message="Login é obrigatório!")])
+    senha = PasswordField("Senha", validators=[DataRequired(message="Senha é obrigatória!"),
+                                             Length(min= 4, message="A senha deve ter no mínimo 4 caracteres!")])
+    Confirmacao = PasswordField("Confirme a senha", validators=[EqualTo("Senha", message="Não é igual a senha definida!")])
+    submit = SubmitField("Registrar")
