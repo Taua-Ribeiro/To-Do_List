0ef60d34597564bb3ffe3a392ab450d75c644ae6
Taua Ribeiro <tauaribeiro.ba1@gmail.com>
2026-09-06 10:38:57 -0300

    UPDATE
    Corrigindo o nome da variável confirmação.

--- app/wtf/register_form.py
+++ app/wtf/register_form.py
@@ -6,5 +6,5 @@ class RegisterForm(FlaskForm):
     login = StringField("Login", validators=[DataRequired(message="Login é obrigatório!")])
     senha = PasswordField("Senha", validators=[DataRequired(message="Senha é obrigatória!"),
                                              Length(min= 4, message="A senha deve ter no mínimo 4 caracteres!")])
-    Confirmacao = PasswordField("Confirme a senha", validators=[EqualTo("Senha", message="Não é igual a senha definida!")])
+    confirmacao = PasswordField("Confirme a senha", validators=[EqualTo("Senha", message="Não é igual a senha definida!")])
     submit = SubmitField("Registrar")
