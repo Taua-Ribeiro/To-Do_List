66933bd93ab98d0a7762350adfdad8ae12137727
Taua Ribeiro <tauaribeiro.ba1@gmail.com>
2026-09-06 10:38:03 -0300

    UPDATE
    Atualizando a view de registro.

--- app/auth.py
+++ app/auth.py
@@ -3,24 +3,22 @@ import functools
 from flask import (
     Blueprint,
     g,
-    request,
     session,
     flash,
     redirect,
     render_template,
     url_for,
-    abort
 )
+
 from werkzeug.security import check_password_hash
 
 from app.database import get_session
 from app.services.usuario_service import create_usuario, get_one_usuario
 from app.utils.app_exceptions import ServiceException
 from app.wtf.login_form import LoginForm
+from app.wtf.register_form import RegisterForm
 
 from sqlalchemy import select
-from sqlalchemy.exc import IntegrityError
-
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -44,42 +42,38 @@ def carregar_usuario_logado():
 
             g.usuario = sess.scalars(stmt).one()
 
-@bp.route('/register', methods=['GET', 'POST'])
+@bp.route("/auth", methods=["GET", "POST"])
 def register():
-    method = request.method
-    error = []
+    form = RegisterForm()
 
-    try:
-        if method == 'POST':
-            login = request.form["login"].strip()
-            senha = request.form["senha"].strip()
-            confirmacao = request.form["confirmacao"].strip()
+    if form.validate_on_submit():
+        try:
+            error = False
+            login = form.login.data.strip().lower()
+            senha = form.senha.data.strip()
+            confirmacao = form.confirmacao.data.strip()
 
-            if not login:
-                error.append("Login é necessário!")
-
-            if not senha:
-                error.append("Senha é necessária!")
-
+            if login == "" or senha == "":
+                error = True
+                flash("Login e senha são obrigatórios")
+                
             if len(senha) < 4:
-                error.append("A senha precisa ter pelo menos 4 caracters!")
+                error = True
+                flash("A senha deve possuir pelo menos 4 caracteres!", category="error")
 
             if senha != confirmacao:
-                error.append("A senha não é igual à confirmação da senha!")
+                error = True
+                flash("A confirmação da senha não é igual a senha inserida!", category="error")
+            
+            create_usuario(login, senha)
 
-            if len(error) == 0:
-                create_usuario(login, senha)
+            if not error:
+                flash("Usuário registrado com sucesso!", category="success")
+                return render_template('auth/login.html')
+        except ServiceException as e:
+            flash(e.message, category="error")
 
-                flash("Usuário criado com sucesso!", "sucesso")
-
-                return redirect(url_for('auth.login'))
-        elif method != "GET":
-            return abort(405, f'Método {method} não é permitido!')
-
-        return render_template('auth/register.html', error=error)
-    except ServiceException as e:
-        return render_template('auth/register.html', error= [e.message])
-
+    return render_template("auth/register.html", form= form)
 
 @bp.route('/logout')
 def logout():
@@ -93,13 +87,27 @@ def login():
 
     if form.validate_on_submit():
         try:
-            login_usuario = form.login.data
-            senha_usuario = form.senha.data
+            error = False
+            login = form.login.data.strip().lower()
+            senha = form.senha.data.strip()
 
-            create_usuario(login_usuario, senha_usuario)
+            if login == "" or senha == "":
+                error = True
+                flash("Login e senha são obrigatórios", category="error")
 
-            # Trocar para renderizar o template de registro
-            return render_template('auth/login.html')
+            if len(senha) < 4:
+                error = True
+                flash("A senha precisa ter no mínimo 4 caracteres!", category="error")
+
+            usuario = get_one_usuario(login)
+
+            if not check_password_hash(usuario.hash_senha, senha):
+                error = True
+                flash("Usuário e/ou senha inválidos!", category="error")
+
+            if not error:
+                # Trocar para renderizar o index
+                return render_template('auth/login.html')
         except ServiceException as e:
             flash(e.message, category='error')
     
